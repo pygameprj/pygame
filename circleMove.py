6dfc379a7e24ebda683b150293d870c9dc5eb8b2
SeongJae Kim <72365940+kimsj8616@users.noreply.github.com>
2023-03-07 10:43:36 +0900

    Update circleMove.py
    주석 추가 및 상태 바꿀때 속도 값 안 즉시 적용되도록 수정

--- circleMove.py
+++ circleMove.py
@@ -20,52 +20,60 @@ pos_y = SCREEN_HEIGHT/2 #시작 y좌표
 count = 0 # 같은 상태 몇번 했는지
 clock = pygame.time.Clock() #주기
 while True:
-    clock.tick(60) #주기 
-    for event in pygame.event.get():
+    clock.tick(60) #주기 마다 입력 키값 받음
+    for event in pygame.event.get(): #종료키 입력 되면 종료(컨트롤 + C)
         if event.type == pygame.QUIT:
             sys.exit()
 
     key_event = pygame.key.get_pressed() #무슨 키 입력했는지
     if key_event[pygame.K_LEFT]: #왼쪽
-        if STATE == pygame.K_LEFT: #count++
+        if STATE == pygame.K_LEFT: #이전 상태와 같은 상태면 count++
             count += 1
         else: count = 0
-        STATE = pygame.K_LEFT #이전상태
+        
+        STATE = pygame.K_LEFT #상태
         S = 1 + (count//10)*2   #속도
-        pos_x -= S
+        pos_x -= S  #이동
+
         if pos_x <= 0 + R: # 화면 넘어가지 않는 부분
             pos_x = 0 + R
 
     if key_event[pygame.K_RIGHT]: #오른쪽
-        if STATE == pygame.K_RIGHT:
+        if STATE == pygame.K_RIGHT: #이전 상태와 같은 상태면 count++
             count += 1
         else: count = 0
-        STATE = pygame.K_RIGHT
+
+        STATE = pygame.K_RIGHT #상태
         S = 1 + (count//10)*2   #속도
-        pos_x += S
-        if pos_x >= SCREEN_WIDTH - R:
+        pos_x += S  #이동
+
+        if pos_x >= SCREEN_WIDTH - R: # 화면 넘어가지 않는 부분
             pos_x = SCREEN_WIDTH - R
 
     if key_event[pygame.K_UP]: #위쪽
-        if STATE == pygame.K_UP:
+        if STATE == pygame.K_UP: #이전 상태와 같은 상태면 count++
             count += 1
         else: count = 0
-        STATE = pygame.K_UP
+
+        STATE = pygame.K_UP #상태
         S = 1 + (count//10)*2   #속도
-        pos_y -= S
-        if pos_y <= 0 + R:
+        pos_y -= S  #이동
+
+        if pos_y <= 0 + R: # 화면 넘어가지 않는 부분
             pos_y = R
 
     if key_event[pygame.K_DOWN]:    #아래쪽
-        if STATE == pygame.K_DOWN:
+        if STATE == pygame.K_DOWN: #이전 상태와 같은 상태면 count++
             count += 1
         else: count = 0
-        STATE = pygame.K_DOWN
+
+        STATE = pygame.K_DOWN #상태
         S = 1 + (count//10)*2   #속도
-        pos_y += S
-        if pos_y >= SCREEN_HEIGHT - R:
+        pos_y += S  #이동
+
+        if pos_y >= SCREEN_HEIGHT - R: # 화면 넘어가지 않는 부분
             pos_y = SCREEN_HEIGHT - R
 
-    screen.fill(BLACK)
-    pygame.draw.circle(screen, GREEN, (pos_x, pos_y), R)
-    pygame.display.update()
+    screen.fill(BLACK)  #배경 색 검은색으로 그리기
+    pygame.draw.circle(screen, GREEN, (pos_x, pos_y), R)    #원 그리기
+    pygame.display.update() #화면 업데이트

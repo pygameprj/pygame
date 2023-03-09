22a383e5920d83f58f9ee49ddbfda211971c84db
kimboxu <kimboxu@gmail.com>
2023-03-09 13:54:46 +0900

    변수명 직관적으로 수정 및 main 함수 추가

--- Pygame_t2.py
+++ Pygame_t2.py
@@ -3,19 +3,16 @@
 import pygame
 import random
 
-# 1. 게임 초기화
-pygame.init()
+#전역 변수
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0,255, 0)
 
-# 2. 게임창 옵션 설정
-size = [400, 900]
-screen = pygame.display.set_mode(size)
+SCREEN_WIDTH = 400 #가로
+SCREEN_HEIGHT = 900 #세로
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-title = "My Game"
-pygame.display.set_caption(title)
-
-# 3. 게임내 필요한 설정
-clock = pygame.time.Clock()
-
+#오브젝트 클래스
 class obj:
     def __init__(self):
         self.x = 0
@@ -36,98 +33,112 @@ class obj:
     def show(self):
         screen.blit(self.img, (self.x, self.y))
 
+def main():
+    # 1. 게임 초기화
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-ss = obj()  # SpaceShip
-ss.put_img("image\good.png")
-ss.change_size(80, 100)
+    # 2. 게임창 옵션 설정
 
-ss.x = round(size[0] / 2 - ss.sx / 2)
-ss.y = size[1] - ss.sy - 15
+    title = "My Game"
+    pygame.display.set_caption(title)
 
-ss.move = 7
+    # 3. 게임내 필요한 설정
+    clock = pygame.time.Clock()
+    count = 0 #미사일 지연
 
-m_list = []
-a_list = []
+    player = obj()  # SpaceShip
+    player.put_img("image\good.png")
+    player.change_size(80, 100)
 
-black = (0, 0, 0)
-white = (255, 255, 255)
+    player.x = round(SCREEN_WIDTH / 2 - player.sx / 2)
+    player.y = SCREEN_HEIGHT - player.sy - 15
 
-# 4. 메인 이벤트
-k = 0 #미사일 지연
-while True:
-     # 4-1, FPS 설정
-    clock.tick(60)
+    player.move = 7
 
-    # 4-2, 각종 입력 감지
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            # 5 게임종료
-            pygame.quit()
+    misile_list = []
+    obstacle_list = []
 
-    key_event = pygame.key.get_pressed()
-    if key_event[pygame.K_LEFT]:
-        ss.x -= ss.move
-        if ss.x <= 0:
-            ss.x = 0
+    # 4. 메인 이벤트
+    while True:
+        # 4-1, FPS 설정
+        clock.tick(60)
+        # 4-2, 각종 입력 감지
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit(0)
 
-    if key_event[pygame.K_RIGHT]:
-        ss.x += ss.move
-        if ss.x >= size[0] - ss.sx:
-            ss.x = size[0] - ss.sx
+        #4-3키입력 움직임
+        key_event = pygame.key.get_pressed()
+        if key_event[pygame.K_LEFT]:
+            player.x -= player.move
+            if player.x <= 0:
+                player.x = 0
 
-    if key_event[pygame.K_SPACE] and k % 6 == 0:
-        mm = obj()  # misile
-        mm.put_img("image\gbad.png")
-        mm.change_size(15, 20)
+        if key_event[pygame.K_RIGHT]:
+            player.x += player.move
+            if player.x >= SCREEN_WIDTH - player.sx:
+                player.x = SCREEN_WIDTH - player.sx
 
-        mm.x = round(ss.x + ss.sx / 2 - mm.sx / 2)
-        mm.y = ss.y - mm.y - 10
-        mm.move = 15
-        m_list.append(mm)
-    k += 1
+        if key_event[pygame.K_SPACE] and count % 6 == 0:
+            count = 0
+            misile = obj()  # misile
+            misile.put_img("image\gbad.png")
+            misile.change_size(15, 20)
 
-    d_list = []
-    for i in range(len(m_list)):
-        m = m_list[i]
-        m.y -= m.move
+            misile.x = round(player.x + player.sx / 2 - misile.sx / 2)
+            misile.y = player.y - misile.y - 10
+            misile.move = 15
+            misile_list.append(misile)
+        count += 1
 
-        if m.y <= -m.sy:
-            d_list.append(i)
-    
-    for d in d_list:
-        del m_list[d]
+        #오브젝트 삭제
+        delObj_list = []
+        for i in range(len(misile_list)):
+            misile = misile_list[i]
+            misile.y -= misile.move
 
-    if random.random() > 0.98: # 랜덤하게 생성 되는 장애물
-        aa = obj()  # devil
-        aa.put_img("image\direction.png")
-        aa.change_size(50, 50)
-
-        aa.x = random.randrange(0, size[0] - aa.sx - round(ss.sx / 2))
-        aa.y = 10
-        aa.move = 1
-
-        a_list.append(aa)
+            if misile.y <= -misile.sy:
+                delObj_list.append(i)
         
-    # 장애물 삭제 되는 부분
-    d_list = []
-    for i in range(len(a_list)):
-        a = a_list[i]
-        a.y += a.move
+        for delObj in delObj_list:
+            del misile_list[delObj]
 
-        if a.y >= size[1]:
-            d_list.append(i)
-    
-    for d in d_list:
-        del a_list[d]
+        # 랜덤하게 생성 되는 장애물
+        if random.random() > 0.98:
+            obstacle = obj()  # devil
+            obstacle.put_img("image\direction.png")
+            obstacle.change_size(50, 50)
 
-    # 4-4, 그리기       
-    screen.fill(black)
-    ss.show()
+            obstacle.x = random.randrange(0, SCREEN_WIDTH - obstacle.sx - round(player.sx / 2))
+            obstacle.y = 10
+            obstacle.move = 1
 
-    for m in m_list:
-        m.show()
-    for a in a_list: 
-        a.show()
+            obstacle_list.append(obstacle)
+            
+        # 장애물 삭제 되는 부분
+        delObj_list = []
+        for i in range(len(obstacle_list)):
+            obstacle = obstacle_list[i]
+            obstacle.y += obstacle.move
 
-    # 4-5, 업데이트
-    pygame.display.flip()
+            if obstacle.y >= SCREEN_HEIGHT:
+                delObj_list.append(i)
+        
+        for delObj in delObj_list:
+            del obstacle_list[delObj]
+
+        # 4-4, 그리기       
+        screen.fill(BLACK)
+        player.show()
+
+        for misile in misile_list:
+            misile.show()
+        for obstacle in obstacle_list: 
+            obstacle.show()
+
+        # 4-5, 업데이트
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()

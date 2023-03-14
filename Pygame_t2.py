125219bdd6d16ec788d38f9cef99d0c292d59d6c
kimboxu <kimboxu@gmail.com>
2023-03-14 16:09:17 +0900

    초기 변수 클래스 생성 및 일시정지 추가

--- Pygame_t2.py
+++ Pygame_t2.py
@@ -24,21 +24,6 @@ laserGunSound = ["sound\laserGun.mp3", "C:\code\pygame\sound\laserGun.mp3"]
 gameOverSound = ["sound\gameOver.mp3", "C:\code\pygame\sound\gameOver.mp3"]
 downHPSound = ["sound\downHP.mp3", "C:\code\pygame\sound\downHP.mp3"]
 
-player_HP = 10
-player_SPEED = 7
-missile_SPEED = 15
-obs_HP = 4
-
-#objSize
-playerSizeX = 80
-playerSizeY = 100
-missileSizeX = 15
-missileSizeY = 20
-obsSizeX = 50
-obsSizeY = 50
-boomX = 50
-boomY = 50
-
 SCREEN_WIDTH = 400 #가로
 SCREEN_HEIGHT = 900 #세로
 
@@ -65,41 +50,60 @@ class obj:
     def show(self, screen):
         screen.blit(self.img, (self.x, self.y))
 
-def initPlayer(player):
+class initVar:
+    def __init__(self):
+        self.player_HP = 10
+        self.player_SPEED = 7
+        self.missile_SPEED = 15
+        self.obs_HP = 4
+
+        #objSize
+        self.playerSizeX = 80
+        self.playerSizeY = 100
+        self.missileSizeX = 15
+        self.missileSizeY = 20
+        self.obsSizeX = 50
+        self.obsSizeY = 50
+        self.boomX = 50
+        self.boomY = 50
+
+        self.obsCount = 0
+        self.m_STR = 1
+def initPlayer(player, init_var):
     player.put_img(playerFile[addressTF])
-    player.change_size(playerSizeX, playerSizeY)
+    player.change_size(init_var.playerSizeX, init_var.playerSizeY)
     player.x = round((SCREEN_WIDTH - player.sx)/2)
     player.y = SCREEN_HEIGHT - player.sy - 15
-    player.move = player_SPEED
-    player.HP = player_HP
+    player.move = init_var.player_SPEED
+    player.HP = init_var.player_HP
 
-def newMissile(m_list, m_STR, player):
+def newMissile(m_list, init_var, player):
     missile = obj()  # missile
     missile.put_img(missileFile[addressTF])
-    missile.change_size(missileSizeX, missileSizeY)
+    missile.change_size(init_var.missileSizeX, init_var.missileSizeY)
     missile.x = round(player.x + player.sx/2 - missile.sx/2)
     missile.y = player.y - missile.y
-    missile.move = missile_SPEED
-    missile.STR = m_STR
+    missile.move = init_var.missile_SPEED
+    missile.STR = init_var.m_STR
     m_list.append(missile)
 
-def newobs(obs_list, obs_SPEED = 2):
+def newobs(obs_list, init_var, obs_SPEED = 2):
     if random.random() < M_PER/100:
         obstacle = obj()  # devil
         obstacle.put_img(obsFile[addressTF])
-        obstacle.change_size(obsSizeX, obsSizeY)
+        obstacle.change_size(init_var.obsSizeX, init_var.obsSizeY)
         obstacle.x = random.randrange(0, SCREEN_WIDTH - obstacle.sx)
         obstacle.y = 10 + 40
         obstacle.move = obs_SPEED
-        obstacle.HP = obs_HP
+        obstacle.HP = init_var.obs_HP
         obs_list.append(obstacle)
 
-def makeBoom(boom_list, obj_list, delObj_list):
+def makeBoom(boom_list, obj_list, delObj_list, init_var):
     for i in range(len(delObj_list)):
         obj_list[delObj_list[i]]
         boom = obj()
         boom.put_img(boomFile[addressTF])
-        boom.change_size(boomX, boomY)
+        boom.change_size(init_var.boomX, init_var.boomY)
         boom.x = obj_list[delObj_list[i]].x
         boom.y = obj_list[delObj_list[i]].y
         boom.HP = 20 # 폭발 이펙트 지속 시간, 약 0.3초
@@ -153,7 +157,7 @@ def outObj(obj_list, player):
     return count
 
 #충돌
-def hitObs(m_list, obs_list, boom_list):
+def hitObs(m_list, obs_list, boom_list, init_var):
     TF = False
     delObs_list = []; delM_list = []
 
@@ -164,7 +168,7 @@ def hitObs(m_list, obs_list, boom_list):
                 if  m_list[i].HP <= 0 and i not in delM_list: delM_list.append(i) 
                 if obs_list[j].HP <= 0 and j not in delObs_list: delObs_list.append(j)
                 TF = True
-    makeBoom(boom_list, obs_list, delObs_list)
+    makeBoom(boom_list, obs_list, delObs_list, init_var)
     global obsCount
     obsCount += len(delObs_list)
     delObjf(obs_list, delObs_list)
@@ -172,31 +176,48 @@ def hitObs(m_list, obs_list, boom_list):
     
     return TF
 
-def hitPlayer(player, obs_list):
+def hitPlayer(player, obs_list, init_var):
     TF = False; delObs_list = []
     for j in range(len(obs_list)):
         if oLap(obs_list[j], player):
-            obs_list[j].HP -= obs_HP
+            obs_list[j].HP -= init_var.obs_HP
             if obs_list[j].HP <= 0 and j not in delObs_list: delObs_list.append(j)
             TF = True
     delObjf(obs_list, delObs_list)
     return TF
 
 #그리기 함수
-def drawInit(screenInit, state = 0):
-    screenInit.fill(BLACK)
+def drawInit(screen, state = 0):
+    screen.fill(BLACK)
     # if state == 0:
-    #     writeScore(screenInit, "게임 시작", 120, 300, color = WHITE, size = 40)
-    #     writeScore(screenInit, "설정", 160, 460, color = WHITE, size = 40)
+    #     writeScore(screen, "게임 시작", 120, 300, color = WHITE, size = 40)
+    #     writeScore(screen, "설정", 160, 460, color = WHITE, size = 40)
     if state == 0:
-        writeScore(screenInit, "게임 시작", 120, 300, color = WHITE, size = 60)
-        writeScore(screenInit, "설정", 160, 460, color = WHITE, size = 40)
+        writeScore(screen, "게임 시작", 120, 300, color = WHITE, size = 60)
+        writeScore(screen, "설정", 160, 460, color = WHITE, size = 40)
     elif state == 1: 
-        writeScore(screenInit, "게임 시작", 120, 300, color = WHITE, size = 40)
-        writeScore(screenInit, "설정", 160, 460, color = WHITE, size = 60)
+        writeScore(screen, "게임 시작", 120, 300, color = WHITE, size = 40)
+        writeScore(screen, "설정", 160, 460, color = WHITE, size = 60)
     return state
 
-def initDisplay(screenInit, clock):
+def drawStop(screen, state = 0):
+    screen.fill(BLACK)
+    if state == 0:
+        writeScore(screen, "게임 재개", 120, 300, color = WHITE, size = 60)
+        writeScore(screen, "처음 으로", 140, 460, color = WHITE, size = 40)
+        writeScore(screen, "게임 종료", 140, 620, color = WHITE, size = 40)
+    elif state == 1:
+        writeScore(screen, "게임 재개", 120, 300, color = WHITE, size = 40)
+        writeScore(screen, "처음 으로", 140, 460, color = WHITE, size = 60)
+        writeScore(screen, "게임 종료", 140, 620, color = WHITE, size = 40)
+    elif state == 2:
+        writeScore(screen, "게임 재개", 120, 300, color = WHITE, size = 40)
+        writeScore(screen, "처음 으로", 140, 460, color = WHITE, size = 40)
+        writeScore(screen, "게임 종료", 140, 620, color = WHITE, size = 60)
+    return state
+    
+def initDisplay(screen, clock):
+    time.sleep(0.2)
     state = 0
     while True:
         clock.tick(60)
@@ -204,7 +225,7 @@ def initDisplay(screenInit, clock):
             if event.type == pygame.QUIT:
                 exit(0)
         key_event = pygame.key.get_pressed()
-        if drawInit(screenInit, state) == 0 and (key_event[pygame.K_KP_ENTER]):
+        if drawInit(screen, state) == 0 and (key_event[pygame.K_KP_ENTER]):
             break
         if key_event[pygame.K_DOWN] and state < 1:
             state +=1
@@ -212,25 +233,47 @@ def initDisplay(screenInit, clock):
             state -= 1
         pygame.display.flip()
 
-def drawGame(screenGame, player, m_list, obs_list, boom_list):
-    screenGame.fill(BLACK)
-    player.show(screenGame)
+def stopDisplay(screen, clock):
+    state = 0
+    while True:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit(0)
+        key_event = pygame.key.get_pressed()
+        if drawStop(screen, state) == 0 and key_event[pygame.K_KP_ENTER] or key_event[pygame.K_ESCAPE]:
+            return 0
+        elif drawStop(screen, state) == 1 and (key_event[pygame.K_KP_ENTER]):
+            return 1
+        elif drawStop(screen, state) == 2 and (key_event[pygame.K_KP_ENTER]):
+            exit(0)
+        elif key_event[pygame.K_DOWN] and state < 2:
+            state +=1
+            time.sleep(0.2)
+        elif key_event[pygame.K_UP] and state > 0:
+            state -= 1
+            time.sleep(0.2)
+        pygame.display.flip()
+
+def drawGame(screen, player, m_list, obs_list, boom_list):
+    screen.fill(BLACK)
+    player.show(screen)
     delBoom_list = []
 
     for missile in m_list:
-        missile.show(screenGame)
+        missile.show(screen)
     for obstacle in obs_list: 
-        obstacle.show(screenGame)
+        obstacle.show(screen)
 
     for i in range(len(boom_list)):
         boom_list[i].HP -= 1
         if boom_list[i].HP > 0:
-            boom_list[i].show(screenGame)
+            boom_list[i].show(screen)
         elif boom_list[i] not in boom_list: delBoom_list.append(boom_list[i])
 
     delObjf(delBoom_list, [i for i in range(len(delBoom_list))])
 
-def mainKey_event(player, missile_Sound, missile_list, missile_STR, count):
+def mainKey_event(player, missile_Sound, missile_list, obstacle_list, boom_list, missile_STR, count, screen, clock, init_var):
     # 4-2, 각종 입력 감지
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -252,16 +295,24 @@ def mainKey_event(player, missile_Sound, missile_list, missile_STR, count):
     #스패이스바 입력시 미사일 생성
     if key_event[pygame.K_SPACE] and count % 6 == 0:
         missile_Sound.play()
-        newMissile(missile_list, missile_STR, player)
+        newMissile(missile_list, init_var, player)
         count = 0
-    # if key_event[pygame.K_ESCAPE]:
-  
+    if key_event[pygame.K_ESCAPE]:
+        pygame.mixer.music.stop()
+        time.sleep(0.2)
+        if stopDisplay(screen, clock):
+            initDisplay(screen, clock) #Init display
+            missile_list.clear(), obstacle_list.clear(), boom_list.clear(); global damageAmount; damageAmount = 0
+            initPlayer(player, init_var) #player state init
+        pygame.mixer.music.load(happythemeBGM[addressTF])
+        pygame.mixer.music.play(-1)
+
 #게임 오버
-def ifGameOver(screenGame, player, size = 40):
+def ifGameOver(screen, player, size = 40):
     if player.HP <= 0:
         font = pygame.font.Font(fontFile[addressTF], size)
         text = font.render("GAME OVER!", True, RED)
-        screenGame.blit(text,(80, SCREEN_HEIGHT/2))
+        screen.blit(text,(80, SCREEN_HEIGHT/2))
         pygame.mixer.music.stop()
         pygame.mixer.Sound(gameOverSound[addressTF]).play()
         pygame.display.flip()
@@ -281,8 +332,9 @@ def main():
     pygame.mixer.music.load(happythemeBGM[addressTF])
     pygame.mixer.music.play(-1)
     missile_Sound = pygame.mixer.Sound(laserGunSound[addressTF])
-    screenInit = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screenGame = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    init_var = initVar()
+
 
     # 2. 게임창 옵션 설정
     title = "My Game"
@@ -297,9 +349,10 @@ def main():
     missile_STR = 1 #미사일 공격력
     obs_SPEED = 2 # 장애물 속도
 
-    initDisplay(screenInit, clock) #Init display
+
+    initDisplay(screen, clock) #Init display
     player = obj()  # SpaceShip
-    initPlayer(player) #player state init
+    initPlayer(player, init_var) #player state init
 
     # 4. 메인 이벤트
     while True:
@@ -307,11 +360,11 @@ def main():
         # 4-1, FPS setting
         clock.tick(60)
         #check key_event
-        mainKey_event(player, missile_Sound, missile_list, missile_STR, count)
+        mainKey_event(player, missile_Sound, missile_list, obstacle_list, boom_list, missile_STR, count, screen, clock, init_var)
 
         # random new obstacle
         if obsCount/10: obs_SPEED = 2 + obsCount/10
-        newobs(obstacle_list, obs_SPEED)
+        newobs(obstacle_list, init_var, obs_SPEED)
 
         #move obj
         UPObjf(missile_list)
@@ -321,17 +374,17 @@ def main():
         outObj(obstacle_list, player)
 
         #충돌시 오브젝트 파괴
-        if hitObs(missile_list, obstacle_list, boom_list):  damageAmount += 1
+        if hitObs(missile_list, obstacle_list, boom_list, init_var):  damageAmount += 1
 
         #if player when hit, down HP
-        if hitPlayer(player, obstacle_list): downHP(player)
+        if hitPlayer(player, obstacle_list, init_var): downHP(player)
 
         # 4-4, draw Display
-        drawGame(screenGame, player, missile_list, obstacle_list, boom_list)
-        writeScore(screenGame, "총 딜량", 10, 0, count = damageAmount)
-        writeScore(screenGame, "처리한 악마 수", 10, 20, count = obsCount)
-        writeScore(screenGame, "player HP", SCREEN_WIDTH-140, 00, count = player.HP, color =  GREEN)
-        if ifGameOver(screenGame, player): break
+        drawGame(screen, player, missile_list, obstacle_list, boom_list)
+        writeScore(screen, "총 딜량", 10, 0, count = damageAmount)
+        writeScore(screen, "처리한 악마 수", 10, 20, count = obsCount)
+        writeScore(screen, "player HP", SCREEN_WIDTH-140, 00, count = player.HP, color =  GREEN)
+        if ifGameOver(screen, player): break
         
         # 4-5, Display update
         pygame.display.flip()
